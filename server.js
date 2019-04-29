0978da028941c210c75fd24836fc23df7d5523d9
luisfrias2 <luisfrias@seattleacademy.org>
2019-04-29 09:15:21 -0700

    add index.html to serve test

--- server.js
+++ server.js
@@ -6,22 +6,11 @@ const port = 1211
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.use(express.static('public'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 var gsjson = require('google-spreadsheet-to-json');
-
-gsjson({
-        spreadsheetId: '1-vt_y5hG8fINKqlvQJGBD4Ea1qKeC5Ufc-WYMGE9_Zg',
-        // other options...
-    })
-    .then(function(result) {
-        console.log(result.length);
-        console.log(result);
-    })
-    .catch(function(err) {
-        console.log(err.message);
-        console.log(err.stack);
-    });
 
 app.get('/json', (req, res) => {
     gsjson({
