b4fe39454e674ff651df05f373d0bf4081b0a23d
luisfrias2 <luisfrias@seattleacademy.org>
2019-04-29 09:06:44 -0700

    added express server for the google sheets

--- server.js
+++ server.js
@@ -1,1 +1,41 @@
-console.log('hello amigo')
+// console.log('hello amigo')
+
+const express = require('express')
+const app = express()
+const port = 1211
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+var gsjson = require('google-spreadsheet-to-json');
+
+gsjson({
+        spreadsheetId: '1-vt_y5hG8fINKqlvQJGBD4Ea1qKeC5Ufc-WYMGE9_Zg',
+        // other options...
+    })
+    .then(function(result) {
+        console.log(result.length);
+        console.log(result);
+    })
+    .catch(function(err) {
+        console.log(err.message);
+        console.log(err.stack);
+    });
+
+app.get('/json', (req, res) => {
+    gsjson({
+        spreadsheetId: '1-vt_y5hG8fINKqlvQJGBD4Ea1qKeC5Ufc-WYMGE9_Zg',
+        // other options...
+    })
+    .then(function(result) {
+        console.log(result.length);
+        console.log(result);
+		res.send(result);
+
+    })
+    .catch(function(err) {
+        console.log(err.message);
+        console.log(err.stack);
+    });
+})
